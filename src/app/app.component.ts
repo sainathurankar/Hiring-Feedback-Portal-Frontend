19381a768457b557dd89d7241940176b569a0c20
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2022-05-22 00:54:53 +0530

    adding preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  constructor(private renderer: Renderer2){}
+  ngAfterViewInit(): void {
+    let loader = this.renderer.selectRootElement('#loader');
+    this.renderer.setStyle(loader, 'display', 'none');
+  }
   title = 'client';
 }
